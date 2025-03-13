0dd909b087e981e90bb685bd7f7f1d419eacfda8
Virna Almirah <virnalmirah@gmail.com>
2025-03-13 22:53:59 +0700

    ganti

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Web Developer", "Informatics Engineering Student"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
+import {renderHTML,onClick,setInner} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
+
+renderHTML('card', 'content/index.html')
+
+onClick("instagram",myInstagram);
+   function myInstagram() {
+       window.open('https://www.instagram.com/virnaalll?igsh=bnRua2llb2UzZzZ0', '_blank');
+   }
+onClick("github",myGithub);
+   function myGithub() {
+       window.open('https://github.com/VirnaAlmirah', '_blank');
+   }
+onClick("whatsApp",myWhatsApp);
+   function myWhatsApp() {
+       window.open('https://wa.me/6282116301579', '_blank');
+   }
+
+setInner("text", "Kontak Saya");
